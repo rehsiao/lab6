4889065fe18d782a0c4d1bbc244e19dc539e3e4a
rehsiao <rehsiao@ucsd.edu>
2020-02-13 17:40:59 -0800

    finishedd

--- public/js/facebook.js
+++ public/js/facebook.js
@@ -5,15 +5,12 @@ function checkLoginState() {
 }
 
 function statusChangeCallback(response) {
-  console.log('Facebook login status changed.');
-  console.log(response);
-  // The response object is returned with a status field that lets the
-  // app know the current login status of the person.
-  // Full docs on the response object can be found in the documentation
-  // for FB.getLoginStatus().
   if (response.status === 'connected') {
-    // Logged into your app and Facebook.
-        console.log('Successfully logged in with Facebook');
-         FB.api('/me?fields=name,first_name,picture.width(480)', changeUser);
+    console.log('Successfully logged in with Facebook');
+    FB.api('/me?fields=name,first_name,picture.width(480)', changeUser);
+  }
+
+  function changeUser(response) {
+    //Add code to change name and image
   }
 }
